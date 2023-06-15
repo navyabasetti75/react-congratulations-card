636efb15e60f60254753b2e39df10247be9d5fc5
navya <navyabasetti75@gmail.com>
2023-06-15 15:07:22 +0000

    congratulations card

---
+++ index.js
@@ -0,0 +1,13 @@
+const element = (
+  <div className="container">
+    <h1>Congratulations</h1>
+    <div class="container1">
+      <img src="https://assets.ccbp.in/frontend/react-js/congrats-card-profile-img.png" />
+      <h1 className="heading">Kiran V</h1>
+      <p>Vishnu Institute of computer Education and Technology Bhimavaram</p>
+      <img src="https://assets.ccbp.in/frontend/react-js/congrats-card-watch-img.png" />
+    </div>
+  </div>
+);
+
+ReactDOM.render(element, document.getElementById("root"));
